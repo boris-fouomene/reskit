0e16aa92d32c67f8159c78f8d5d40efd538c6bec
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2024-10-27 10:17:37 +0100

    Update Tooltip component

--- packages/resk-expo/src/components/Tooltip/index.tsx
+++ packages/resk-expo/src/components/Tooltip/index.tsx
@@ -1,7 +1,29 @@
-import { Animated, Dimensions, LayoutChangeEvent, Pressable, StyleSheet, View } from "react-native";
-import React, { forwardRef, useEffect, useRef, useState } from "react";
+import { Pressable } from "react-native";
+import React, { useMemo } from "react";
 import { ITooltipProps } from "./types";
+import { defaultStr, uniqid } from "@resk/core";
+import { useMergeRefs } from "@utils/mergeRefs";
 
-export default function Tooltip({ children, ...props }: ITooltipProps) {
-  return children;
-}
+const Tooltip = React.forwardRef(({
+  children, title, tooltip, disabled, as, testID, id, ...rest
+}: ITooltipProps, ref) => {
+  testID = defaultStr(testID, "RN_TooltipTooltipComponent");
+  // Reference for instance ID or generate a unique one
+  const instanceIdRef = React.useRef(id || uniqid("tippy-instance-id"));
+  // Reference for the child element (e.g., button)
+  const buttonRef = React.useRef(null);
+  // Combine external ref with internal button reference
+  const innerRef = useMergeRefs(ref, buttonRef);
+  const Component = useMemo(() => {
+    return as || Pressable;
+  }, [as]);
+  return (
+    <Component {...rest} disabled={disabled} id={instanceIdRef.current} testID={testID} ref={innerRef}>
+      {children}
+    </Component>
+  );
+});
+
+export default Tooltip;
+
+Tooltip.displayName = "TooltipNative";

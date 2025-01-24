79a5de20d613d471dd36a47ddc6052c20863760e
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-01-24 06:23:37 +0100

    `Update Jest configuration to support TypeScript and updated file paths`

---
+++ jest.config.old.js
@@ -0,0 +1,32 @@
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+const path = require('path');
+//const baseUrl = path.relative(process.cwd(),__dirname).replaceAll("\\","/")
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom : [
+    "**/*.(t|j)s"
+  ],
+  moduleFileExtensions : [
+      "js",
+      "json",
+      "ts",
+      "node"
+ ],
+  roots: ['<rootDir>/src'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { 
+      prefix: '<rootDir>/../..' 
+    }),
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/../..'],
+  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/src/**/*.spec.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.json'
+    }]
+  }
+};

b532c2ebec8ed0165bd1d8b04ca1d9f1f98defc6
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-02-07 04:08:59 +0100

    Global review of nest modules Publishing the first version of resk/nest module

--- packages/resk-nest/src/modules/i18n/i18n.middleware.ts
+++ packages/resk-nest/src/modules/i18n/i18n.middleware.ts
@@ -3,11 +3,52 @@ import { Request, Response, NextFunction } from 'express';
 import { applyLanguage } from './utils';
 import { I18n } from '@resk/core';
 
+/**
+ * A NestJS middleware class that handles internationalization (i18n) for incoming requests.
+ * 
+ * This class implements the NestMiddleware interface and is responsible for setting the locale
+ * based on the 'Accept-Language' header in the incoming request.
+ * 
+ * @example
+ * ```typescript
+ * // Example usage in a NestJS module
+ * import { Module } from '@nestjs/common';
+ * import { I18nMiddleware } from '@resk/nest';
+ * 
+ * export class AppModule implements NestMiddleware {
+ *   configure(consumer: MiddlewareConsumer) {
+ *     consumer.apply(I18nMiddleware).forRoutes('*');
+ *   }
+ * }
+ * ```
+ */
 @Injectable()
 export class I18nMiddleware implements NestMiddleware {
+    /**
+     * Creates an instance of I18nMiddleware.
+     * 
+     * @param i18n The I18n instance injected through the 'I18N' token.
+     */
     constructor(@Inject("I18N") private readonly i18n: I18n) { }
+
+    /**
+     * Handles incoming requests and sets the locale based on the 'Accept-Language' header.
+     * 
+     * @param req The incoming request object.
+     * @param res The response object.
+     * @param next The next function in the middleware chain.
+     * 
+     * @returns A promise that resolves when the locale has been set.
+     */
     async use(req: Request, res: Response, next: NextFunction) {
+        /**
+         * Applies the language to the I18n instance based on the 'Accept-Language' header in the request.
+         */
         await applyLanguage(this.i18n, req);
+
+        /**
+         * Calls the next function in the middleware chain.
+         */
         next();
     }
 }
--- packages/resk-nest/src/modules/i18n/utils.ts
+++ packages/resk-nest/src/modules/i18n/utils.ts
@@ -1,17 +1,47 @@
 import { defaultStr, I18n } from "@resk/core";
 
+/**
+ * Applies the language to the I18n instance based on the 'Accept-Language' header in the request.
+ * 
+ * @param i18n The I18n instance to set the locale for.
+ * @param request The request object containing the 'Accept-Language' header.
+ * 
+ * @returns A promise that resolves when the locale has been set.
+ * 
+ * @example
+ * ```typescript
+ * import { applyLanguage } from './utils';
+ * import { I18n } from '@resk/core';
+ * 
+ * const i18n = new I18n();
+ * const request = { headers: { 'accept-language': 'fr' } };
+ * await applyLanguage(i18n, request);
+ * console.log(i18n.getLocale()); // Output: fr
+ * ```
+ */
 export const applyLanguage = async (i18n: I18n, request: any) => {
     /**
      * Extracts the 'Accept-Language' header from the request and sets the locale accordingly.
      * 
      * If the 'Accept-Language' header is not present, it defaults to 'en'.
+     * 
+     * @note The 'Accept-Language' header is case-insensitive, so both 'accept-language' and 'Accept-Language' are supported.
      */
     let lang = defaultStr(Object.assign({}, request.headers)['accept-language'], Object.assign({}, request.headers)['Accept-Language'], 'en');
+
+    /**
+     * Checks if the extracted language is supported by the I18n instance.
+     * 
+     * If the language is not supported, it defaults to 'en'.
+     */
     if (!i18n.hasLocale(lang)) {
         lang = "en";
     }
-    /** 
+
+    /**
      * Sets the locale using the I18n instance.
+     * 
+     * @note This method is asynchronous and returns a promise that resolves when the locale has been set.
      */
     await i18n.setLocale(lang);
 }

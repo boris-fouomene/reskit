3382b44d0563e050f95d90138f1d6bf532bbd7a4
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-02-28 04:39:02 +0100

    Add : elevation prop to Avatar component

--- packages/resk-native/src/components/Avatar/index.tsx
+++ packages/resk-native/src/components/Avatar/index.tsx
@@ -3,7 +3,7 @@ import { IReactComponent, ITouchableProps } from '@src/types';
 import { useMemo } from 'react';
 import {
   View,
-  ImageProps, ViewProps, 
+  ImageProps, ViewProps,
   Pressable,
   Image,
   StyleSheet,
@@ -15,7 +15,7 @@ import { getTouchableProps } from '@utils/hasTouchHandler';
 import { getIcon, IIconProps, IIconSource } from '@components/Icon';
 import Label, { ILabelProps } from '@components/Label';
 import { IViewProps } from '@components/View';
-import {IThemeColorsTokenName } from '@theme/types';
+import { IThemeColorsTokenName } from '@theme/types';
 import { JSX } from 'react/jsx-runtime';
 import { Colors, useTheme } from '@theme/index';
 
@@ -31,36 +31,36 @@ export interface IAvatarProps<AsProps extends Partial<ITouchableProps> = ViewPro
    *
    *  @default `Press handlers present then Pressable else View`
    */
-   as ?: IReactComponent<Omit<AsProps,'children'> & {children?:JSX.Element}>;
-  
+  as?: IReactComponent<Omit<AsProps, 'children'> & { children?: JSX.Element }>;
+
   /***
     * Color scheme token key to apply a theme color to the avatar.
     * This allows for easy theming and consistency across the application.
   */
-  colorScheme?:IThemeColorsTokenName;
+  colorScheme?: IThemeColorsTokenName;
 
   /** Image source to be displayed on avatar.
    *
    * @default undefined
    */
   source?: ImageSourcePropType;
-  
+
   /***
     * Props for the image displayed on the avatar.
   */
   imageProps?: ImageProps;
-  
+
   /**
    * Displays an icon as the main content of the Avatar. **Cannot be used alongside text**. When used with the `source` prop it will be used as the placeholder.
    * @default undefined
    */
   icon?: IIconSource;
-  
+
   /***
   * Props for the icon displayed on
   */
-  iconProps?:Omit<IIconProps,"name"|"iconName" | "source">;
-  
+  iconProps?: Omit<IIconProps, "name" | "iconName" | "source">;
+
   /**
    * Indicates whether the Avatar is disabled.
    * A disabled avatar will not respond to user interactions and will appear greyed out.
@@ -90,22 +90,27 @@ export interface IAvatarProps<AsProps extends Partial<ITouchableProps> = ViewPro
    * @default small
    */
   size?: ('small' | 'medium' | 'large' | 'xlarge') | number;
-  
+
   /***
     The background color of the avatar.
   */
   backgroundColor?: string;
-  
+
   /***
     The color of the avatar, in case of text avatar or icon avatar.
   */
   color?: string;
-  
+
   children?: JSX.Element;
-  
+
   testID?: string;
-  
+
   style?: ViewStyle;
+
+  /***
+   * The elevation of the avatar, in case of text avatar or icon avatar.
+   */
+  elevation?: number;
 }
 
 /**
@@ -117,7 +122,7 @@ export function Avatar<AsProps extends Partial<ITouchableProps> = ViewProps>({
   icon,
   source,
   size = 'small',
-  rounded:customRounded,
+  rounded: customRounded,
   text,
   children,
   iconProps,
@@ -128,62 +133,65 @@ export function Avatar<AsProps extends Partial<ITouchableProps> = ViewProps>({
   style,
   backgroundColor,
   color,
+  elevation,
   ...rest
 }: IAvatarProps<AsProps>) {
   const rounded = typeof customRounded === 'boolean' ? customRounded : true;
-  const avatarSize = typeof size === 'number' ? size : isNonNullString(size) && typeof avatarSizes[size] ==="number"
-  && avatarSizes[size] || avatarSizes.small;
+  const avatarSize = typeof size === 'number' ? size : isNonNullString(size) && typeof avatarSizes[size] === "number"
+    && avatarSizes[size] || avatarSizes.small;
   const height = avatarSize, width = avatarSize;
   const touchableProps = getTouchableProps(rest);
   const theme = useTheme();
-  imageProps = Object.assign({},imageProps);
-  const {color:cColor,backgroundColor:cBackgroundColor} = Object.assign({},theme.getColorScheme(colorScheme));
+  const elevationStyle = typeof elevation === 'number' && theme.elevations[elevation] || undefined;
+  imageProps = Object.assign({}, imageProps);
+  const { color: cColor, backgroundColor: cBackgroundColor } = Object.assign({}, theme.getColorScheme(colorScheme));
   color = Colors.isValid(color) ? color : cColor;
   backgroundColor = Colors.isValid(backgroundColor) ? backgroundColor : cBackgroundColor;
-  testID = defaultStr(testID,"resk-avatar");
-  const content = useMemo(()=>{
-    if(isNonNullString(text)){
-      return <Label 
-        testID = {testID+"-text"}
-        children ={text}
-        {...Object.assign({},textProps)}
-        style = {[{fontSize:avatarSize/1.8},textProps?.style]}
+  testID = defaultStr(testID, "resk-avatar");
+  const content = useMemo(() => {
+    if (isNonNullString(text)) {
+      return <Label
+        testID={testID + "-text"}
+        children={text}
+        {...Object.assign({}, textProps)}
+        style={[{ fontSize: avatarSize / 1.8 }, textProps?.style]}
       />;
     }
-    if(!icon) return null;
-    return getIcon({icon,size:avatarSize/2,color,testID:testID+"-icon",...Object.assign({},iconProps)});
-  },[avatarSize,colorScheme,rounded,color,backgroundColor,icon,iconProps,textProps,testID,text]);
-  const Component = useMemo(()=>{
-    if(isReactComponent(as) && as){
+    if (!icon) return null;
+    return getIcon({ icon, size: avatarSize / 2, color, testID: testID + "-icon", ...Object.assign({}, iconProps) });
+  }, [avatarSize, colorScheme, rounded, color, backgroundColor, icon, iconProps, textProps, testID, text]);
+  const Component = useMemo(() => {
+    if (isReactComponent(as) && as) {
       return as;
     }
-    if(touchableProps){
-        return Pressable;
+    if (touchableProps) {
+      return Pressable;
     }
     return View;
-  },[as,touchableProps]);
+  }, [as, touchableProps]);
   return (
     <Component
       testID={testID}
       {...rest as AsProps}
-      {...Object.assign({},touchableProps)}
+      {...Object.assign({}, touchableProps)}
       style={StyleSheet.flatten([
         styles.container,
-        { height, width,backgroundColor},
-        rounded ? { borderRadius: width / 2, overflow: 'hidden'} : undefined,
+        { height, width, backgroundColor },
+        rounded ? { borderRadius: width / 2, overflow: 'hidden' } : undefined,
+        elevationStyle,
         style
       ]) as any}
     >
       <>
         {source ? <Image
-            testID={testID+"-image"}
-            source={source}
-            accessibilityLabel={defaultStr(text)}
-            borderRadius={rounded ? width / 2 : undefined}
-            {...imageProps}
-            style={StyleSheet.flatten([styles.avatar,{width,height},imageProps.style])}
-          /> : content}
-          {children}
+          testID={testID + "-image"}
+          source={source}
+          accessibilityLabel={defaultStr(text)}
+          borderRadius={rounded ? width / 2 : undefined}
+          {...imageProps}
+          style={StyleSheet.flatten([styles.avatar, { width, height }, imageProps.style])}
+        /> : content}
+        {children}
       </>
     </Component>
   );
@@ -205,6 +213,6 @@ const styles = StyleSheet.create({
   text: {
     textAlign: 'center',
     zIndex: 1,
-    
+
   },
 });

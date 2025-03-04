7fd92fd579a1df9149d950bb1575ee6421d3d062
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-03-04 06:27:13 +0100

    implementing the native getLocales, returning the available locales from a device

--- packages/resk-native/src/i18n/locales/index.ts
+++ packages/resk-native/src/i18n/locales/index.ts
@@ -1,25 +1,27 @@
-import { isNonNullString } from "@resk/core";
 import { II18nLocale } from "../types";
 import platform from "@platform/index";
+import { parseLanguageTag } from "./utils";
 
 
 export function getLocales(): II18nLocale[] {
-    if (!platform.isClientSide() || typeof navigator === 'undefined' || !navigator || !Array.isArray(navigator?.languages)) return [];
+    if (!platform.isClientSide() || typeof navigator === 'undefined' || !navigator) return [];
     const result: II18nLocale[] = [];
-    navigator.languages.map((language) => {
-        if (!isNonNullString(language)) return undefined;
-        const parts = language.split('-');
-        const languageCode = parts[0];
-        const scriptCode = parts[1] && parts[1].length === 4 ? parts[1] : undefined;
-        const countryCode = parts[parts.length - 1];
-        const isRTL = ['ar', 'he', 'fa', 'ur'].includes(languageCode);
-        result.push({
-            languageCode,
-            scriptCode,
-            countryCode,
-            languageTag: language,
-            isRTL,
+    if (Array.isArray(navigator?.languages)) {
+        navigator.languages.map((language) => {
+            const parsedLanguage = parseLanguageTag(language);
+            if (parsedLanguage) {
+                result.push(parsedLanguage);
+            }
         });
-    });
+        if (result.length) return result;
+    }
+    // Ensure we're in a web environment with the Intl API
+    if (typeof Intl === 'undefined' || !Intl || !Intl.Locale) {
+        const parsedLanguage = parseLanguageTag(navigator?.language);
+        if (parsedLanguage) {
+            return [parsedLanguage];
+        }
+        return [];
+    }
     return result;
 }

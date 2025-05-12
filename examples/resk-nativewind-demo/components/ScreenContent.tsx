b439330e93c17ddd3cfd325f1cdf56afffef0be1
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-05-12 06:34:43 +0100

    fix: Update ScreenContent component styles to include hover effect for improved user interaction

--- examples/resk-nativewind-demo/components/ScreenContent.tsx
+++ examples/resk-nativewind-demo/components/ScreenContent.tsx
@@ -26,7 +26,7 @@ export const ScreenContent = ({ title, path, children }: ScreenContentProps) => 
   );
 };
 const styles = {
-  container: `items-center flex-1 justify-center`,
+  container: `items-center flex-1 justify-center hover:bg-red-500 `,
   separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
   title: `text-xl font-bold text-green-500`,
 };

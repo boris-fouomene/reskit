64213a64f4a0bf95aba384f7ffe722e902eb90f2
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-05-14 08:30:59 +0100

    feat: Enhance IconButton component documentation with detailed prop descriptions

--- packages/resk-nativewind/src/components/Icon/Button.tsx
+++ packages/resk-nativewind/src/components/Icon/Button.tsx
@@ -9,6 +9,26 @@ import { cn } from '@utils/cn';
 
 const PADDING = 8;
 
+/**
+ * A functional component that renders an icon button with customizable properties.
+ * It supports loading states and can display either a font icon or an image icon.
+ *
+ * @param {IIconButtonProps} props - The properties to configure the IconButton.
+ * @param {number} [props.size] - The size of the icon to display.
+ * @param {boolean} [props.disabled] - Whether the button is disabled.
+ * @param {object} [props.style] - The custom styles to apply to the icon.
+ * @param {string} [props.testID] - Optional test identifier for the button.
+ * @param {boolean} [props.isLoading=false] - Indicates if a loading spinner should be shown instead of the icon.
+ * @param {string} [props.iconName] - The name of the font icon to display.
+ * @param {object} [props.source] - The image source for the icon.
+ * @param {string} [props.containerClassName] - Additional class names for the button container.
+ * @param {number} [props.containerSize] - The size of the button container.
+ * @param {string} [props.className] - Additional class names for the icon.
+ * @param {React.Ref} [props.ref] - A reference to the button container.
+ * @param {object} [props.rest] - Additional properties passed to the icon component.
+ *
+ * @returns {JSX.Element} The rendered IconButton component.
+ */
 export default function IconButton(
     {
         size,

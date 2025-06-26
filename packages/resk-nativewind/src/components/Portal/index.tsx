bcf7196b4d74cd6dc2c76e5600143d48061ac8ee
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-06-26 07:26:41 +0100

    refactor(Portal): remove unused useEffect import to clean up code

--- packages/resk-nativewind/src/components/Portal/index.tsx
+++ packages/resk-nativewind/src/components/Portal/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { defaultStr, getMaxZindex } from '@resk/core/utils';
-import { useEffect, useId } from 'react';
+import { useId } from 'react';
 import { createPortal } from 'react-dom';
 import { cn } from '@utils/cn';
 import { styles } from './utils';

842667dff8d17262b7947d83886214439c3b34ba
transfergratisboris <boris.fouomene@transfergratis.com>
2025-07-07 16:36:00 +0100

    fix(Backdrop): update className to use full screen dimensions

--- packages/resk-nativewind/src/components/Backdrop/index.tsx
+++ packages/resk-nativewind/src/components/Backdrop/index.tsx
@@ -7,6 +7,6 @@ export function Backdrop(props: IHtmlDivProps) {
     return <Div
         testID="resk-backdrop"
         {...props}
-        className={cn("resk-backdrop absolute flex-1 w-full h-full left-0 right-0 top-0 bottom-0", props.className)}
+        className={cn("resk-backdrop absolute flex-1 w-screen h-screen left-0 right-0 top-0 bottom-0", props.className)}
     />
 }

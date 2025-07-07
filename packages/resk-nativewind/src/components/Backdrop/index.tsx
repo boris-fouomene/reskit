7b4c7b6e6e386fb1c200128ff5ad27d63b49140c
transfergratisboris <boris.fouomene@transfergratis.com>
2025-07-07 16:36:42 +0100

    fix(Backdrop): remove unused import for cleaner code

--- packages/resk-nativewind/src/components/Backdrop/index.tsx
+++ packages/resk-nativewind/src/components/Backdrop/index.tsx
@@ -1,7 +1,6 @@
 import { Div } from "@html/Div";
 import { IHtmlDivProps } from "@html/types";
 import { cn } from "@utils/cn";
-import { classes } from "@variants/classes";
 
 export function Backdrop(props: IHtmlDivProps) {
     return <Div

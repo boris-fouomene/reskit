ea4e00b5df2534cdd104a3bdcb86ecda1b2b69c9
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-07-18 07:04:48 +0100

    chore: update version to 1.0.1-canary.56 in package.json; refactor Image component to use Div and improve className handling

--- packages/resk-nativewind/src/html/Image/index.tsx
+++ packages/resk-nativewind/src/html/Image/index.tsx
@@ -1,7 +1,7 @@
+import { Div } from "@html/Div";
 import { IHtmlImageProps } from "../types";
-import { normalizeHtmlProps } from "../utils";
-
-export function Image({ defaultSource, ...props }: IHtmlImageProps) {
-    return <img {...normalizeHtmlProps(props)} />;
+import { cn } from "@utils/cn"
+export function Image({ defaultSource, className, ...props }: IHtmlImageProps) {
+    return <Div role="img" {...props} className={cn("resk-image", className)} asHtmlTag="img" />;
 }
 Image.displayName = "Html.Image";

a2ef1daba5a78cffae076629080c27bc3664bfa6
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-07-20 18:32:15 +0100

    feat: update version to 1.0.1-canary.73 and modify backdrop class to use VariantsColors

--- packages/resk-nativewind/src/variants/classes.ts
+++ packages/resk-nativewind/src/variants/classes.ts
@@ -1,3 +1,4 @@
+import { VariantsColors } from "./colors/generated";
 export const classes: IVariantsClasses = {
   disabled: "pointer-events-none opacity-70 web:cursor-not-allowed",
   readOnly: "pointer-events-none opacity-80 web:cursor-not-allowed",
@@ -7,7 +8,7 @@ export const classes: IVariantsClasses = {
   positionFixed: "absolute web:fixed",
   absoluteFill: " absolute web:fixed flex-1 w-full h-full left-0 right-0 top-0 bottom-0",
   cursorDefault: "cursor-default",
-  backdrop: "bg-backdrop dark:bg-dark-backdrop",
+  backdrop: VariantsColors.background.backdrop,
   absoluteFillHidden: "opacity-0 z-0 pointer-events-none",
 };
 export interface IVariantsClasses {

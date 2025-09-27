317930fca003303ce44900f7369a2d57939ac85c
LAPTOP-GA9EKJT2\BORIS <borispipo@gmail.com>
2025-09-27 09:35:08 +0100

    fix: update version to 1.26.1 and enhance tsup configuration for better compatibility and performance

--- packages/resk-core/tsup.ts
+++ packages/resk-core/tsup.ts
@@ -1,38 +1,49 @@
+import { execSync } from "child_process";
+import glob from "fast-glob";
+import { replaceTscAliasPaths } from "tsc-alias";
 import { Options } from "tsup";
-import glob from 'fast-glob';
-import { execSync } from 'child_process';
-import { replaceTscAliasPaths } from 'tsc-alias';
 
-export default function createConfig(type?: "build" | "test" | "dts", options?: Options): Options {
-    options = Object.assign({}, options);
-    const isTest = type === "test";
-    return {
-        entry: glob.sync(`./src/**/${!isTest ? '!(*.d|*.spec|*.test)' : "*"}.(ts|tsx|js|jsx)`),
-        format: ["cjs"],// isTest ? ["cjs"] : ["cjs", "esm"],
-        outDir: "./build",
-        splitting: false, // Avoid splitting to keep module references simple
-        sourcemap: false,
-        clean: true, // Clears dist before building
-        dts: false,
-        minify: true, // Optimizes performance
-        target: "es2015",
-        treeshake: true,
-        bundle: false, // ❌ Disable bundling to keep file structure
-        external: ["node_modules"], // Prevent bundling external dependencies
-        //legacyOutput: true,     // Add explicit file extensions to resolved imports
-        async onSuccess() {
-            console.log("Running tsc-alias after the build to resolve the path aliases");
-            replaceTscAliasPaths({
-                //configFile: isTest ? "tsconfig.test.json" : "tsconfig.json",
-                outDir: isTest ? "./dist" : "./build",
-            })
-            if (!isTest) {
-                execSync('npm run build-dts', { stdio: 'inherit' });
-            }
-            if (typeof options?.onSuccess == "function") {
-                options.onSuccess();
-            }
-        },
-        ...options,
-    };
+export default function createConfig(
+  type?: "build" | "test" | "dts",
+  options?: Options
+): Options {
+  options = Object.assign({}, options);
+  const isTest = type === "test";
+  return {
+    entry: glob.sync(
+      `./src/**/${!isTest ? "!(*.d|*.spec|*.test)" : "*"}.(ts|tsx|js|jsx)`
+    ),
+    format: ["cjs", "esm"], // isTest ? ["cjs"] : ["cjs", "esm"],
+    outDir: "./build",
+    splitting: false, // Avoid splitting to keep module references simple
+    sourcemap: false,
+    clean: true, // Clears dist before building
+    dts: false,
+    minify: true, // Optimizes performance
+    target: "es2015",
+    treeshake: true,
+    bundle: false, // ❌ Disable bundling to keep file structure
+    external: ["node_modules"], // Prevent bundling external dependencies
+    cjsInterop: true, // Enable CJS interop for better compatibility
+    legacyOutput: true, // Add explicit file extensions to resolved imports
+    banner: {
+      js: "Object.defineProperty(exports, '__esModule', { value: true });",
+    },
+    async onSuccess() {
+      console.log(
+        "Running tsc-alias after the build to resolve the path aliases"
+      );
+      replaceTscAliasPaths({
+        //configFile: isTest ? "tsconfig.test.json" : "tsconfig.json",
+        outDir: isTest ? "./dist" : "./build",
+      });
+      if (!isTest) {
+        execSync("npm run build-dts", { stdio: "inherit" });
+      }
+      if (typeof options?.onSuccess == "function") {
+        options.onSuccess();
+      }
+    },
+    ...options,
+  };
 }
